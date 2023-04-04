09caec2430e494e7aecb0696d97953a3d3453631
Jaimin <jaiminds1203@gmail.com>
2023-04-04 13:10:03 +0530

    JatinModifications Accepted + my old updates

--- Placement Cell/server/controller/controller.js
+++ Placement Cell/server/controller/controller.js
@@ -301,3 +301,134 @@ exports.updateUser = async (req, res) =>
 {
 
 }
+
+//company home  by Jaimin
+exports.showJob = (req, res) => {
+    const companyID = null;
+    job.find({ comp: companyID })
+        .then(data => {
+            if (!data) {
+                res.status(404).send({ message: "Not found company with id " + id })
+            } else {
+                res.send(data)
+            }
+        })
+        .catch(err => {
+            res.status(500).send({ message: "Error retrieving company with id " + id })
+        })
+};
+
+exports.postJob = (req, res) => {
+    // add job to jobSchema
+    const companyID = null;
+    if (!req.body) {
+        res.status(400).send({ message: 'Content can not be empty!' });
+        return;
+    }
+
+    // new student
+    const user = new job({
+        comp: req.body.comp,
+        postingLocation: req.body.postingLocation,
+        ugCriteria: req.body.ugCriteria,
+        cpiCriateria: req.body.cpiCriateria,
+        ctc: req.body.ctc,
+        description: req.body.description,
+    })
+
+    // save student in the database
+    user
+        .save(user)
+        .then(data => {
+            // redirect to company home page
+            // res.redirect('/');
+            res.send(user);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || 'Some error occured  while creating a create operation',
+            });
+        });
+};
+
+exports.registredStudentsInJob = (req, res) => {
+    const jobID = null;
+    studentJob.find({ job: jobID })
+        .then(data => {
+            if (!data) {
+                res.status(404).send({ message: "Not found Job with id " + id })
+            } else {
+                // dbo.collection('orders').aggregate([
+                //     { $lookup:
+                //       {
+                //         from: 'products',
+                //         localField: 'product_id',
+                //         foreignField: '_id',
+                //         as: 'orderdetails'
+                //       }
+                //     }
+                //   ]).toArray(function(err, res)
+                // // res.send(data)
+                // code Join job, student and studentJob
+                job.aggregate([
+                    {
+                        $lookup:
+                        {
+                            from: 'studentjobs',
+                            localField: '_id',
+                            foreignField: 'job',
+                            as: 'studentjobsjoinjob'
+                        }
+                    }
+                ]).toArray(function (err, res) {console.log(res)});
+            }
+        })
+        .catch(err => {
+            res.status(500).send({ message: "Error retrieving Job with id " + id })
+        })
+};
+
+exports.jobsRegistredbyStudent = (req, res) => {
+    const jobID = null;
+    studentJob.find({ job: jobID })
+        .then(data => {
+            if (!data) {
+                res.status(404).send({ message: "Not found Job with id " + id })
+            } else {
+                res.send(data)
+            }
+        })
+        .catch(err => {
+            res.status(500).send({ message: "Error retrieving Job with id " + id })
+        })
+};
+
+// Help student to register in Job
+exports.registerStudentInJob = (req, res) => {
+    const jobID = req.params._id;
+    const studentID = req.id;
+    if (!req.body) {
+        res.status(400).send({ message: 'Content can not be empty!' });
+        return;
+    }
+
+    // new student
+    const user = new studentJob({
+        job: jobID,
+        student: studentID,
+    });
+
+    // save student in the database
+    user
+        .save(user)
+        .then(data => {
+            // redirect to company home page
+            // res.redirect('/');
+            res.send(user);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || 'Some error occured  while creating a create operation',
+            });
+        });
+};
